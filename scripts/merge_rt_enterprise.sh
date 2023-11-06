0186badb56aa8729c98f5c18de353ecd1cd2a7bd
Feng Han <hanfeng@dataelem.com>
2023-11-06 10:08:00 +0800

    update elem ocr

--- scripts/merge_rt_enterprise.sh
+++ scripts/merge_rt_enterprise.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 function merge() {
-    RT_ENTERPRISE_DIR="/home/hanfeng/projects3/bisheng-rt-enterprise"
+    RT_ENTERPRISE_DIR="/home/hanfeng/projects/bisheng-rt-enterprise"
     RT_ENTER_RELEASE_DIR="$RT_ENTERPRISE_DIR/tritonbuild/install"
 
     RT_RELEASE_DIR="/opt/bisheng-rt"
-    cp $RT_ENTER_RELEASE_DIR/bin/rtserver $RT_RELEASE_DIR/bin/rtserver.enter
-    cp -fr $RT_ENTER_RELEASE_DIR/lib $RT_RELEASE_DIR/bin/
-    cp -fr $RT_ENTER_RELEASE_DIR/backends $RT_RELEASE_DIR/backends_enterprise
+    # cp $RT_ENTER_RELEASE_DIR/bin/rtserver $RT_RELEASE_DIR/bin/rtserver.ent
+    # cp -fr $RT_ENTER_RELEASE_DIR/lib $RT_RELEASE_DIR/bin/
+    # cp -fr $RT_ENTER_RELEASE_DIR/backends $RT_RELEASE_DIR/backends_enterprise 
+    cp -fr $RT_ENTER_RELEASE_DIR/plugins $RT_RELEASE_DIR/
 }
 
 merge
