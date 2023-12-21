0622875eceaa84ce3b420c7d07eb0ee07ba635ec
hanfeng <hanfeng@dataelem.com>
2023-12-21 19:00:22 +0800

    support baichuan stream

--- python/pybackend_libs/src/tests/test_baichuan_chat.py
+++ python/pybackend_libs/src/tests/test_baichuan_chat.py
@@ -102,6 +102,50 @@ def test_case2():
     print(model.predict(inp3))
 
 
+def test_stream():
+    params = {
+        'pretrain_path': '/public/bisheng/model_repository/Baichuan2-13B-Chat',
+        'devices': '0,2',
+        'gpu_memory': 40,
+        'precision': 'fp16',
+        'max_tokens': 256,
+    }
+    model = BaichuanChat(**params)
+    inp1 = {
+        'model': 'baichua2_13b_chat',
+        'messages': [{
+            'role': 'user',
+            'content': '你是谁'
+        }],
+    }
+
+    inp2 = {
+        'model': 'baichua2_13b_chat',
+        'messages': [{
+            'role': 'user',
+            'content': '你是哪家公司研发的'
+        }],
+    }
+
+    inp3 = {
+        'model': 'baichua2_13b_chat',
+        'messages': [{
+            'role': 'user',
+            'content': '你能做什么'
+        }],
+    }
+
+    for outp in model.stream_predict(inp1):
+        print(outp)
+
+    for outp in model.stream_predict(inp2):
+        print(outp)
+
+    for outp in model.stream_predict(inp3):
+        print(outp)
+
+
 # test_baichuan_13b_chat()
 # test_baichuan2_13b_chat()
-test_case2()
+# test_case2()
+test_stream()
