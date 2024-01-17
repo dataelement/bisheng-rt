fix formula

python/pybackend_libs/src/pybackend_libs/dataelem/model/ocr/latex_det.py
@@ -1,8 +1,111 @@
+# flake8: noqa
 import base64
 from typing import Any, Dict, List
 
 import cv2
 import numpy as np
+import torch
+import torchvision
+
+
+# follow WongKinYiu/yolov7 projects
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2]
+    # where xy1=top-left, xy2=bottom-right
+    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+
+def box_iou(box1, box2):
+    # https://github.com/pytorch/vision/blob/master/torchvision/ops/boxes.py
+    """
+    Return intersection-over-union (Jaccard index) of boxes.
+    Both sets of boxes are expected to be in (x1, y1, x2, y2) format.
+    Arguments:
+        box1 (Tensor[N, 4])
+        box2 (Tensor[M, 4])
+    Returns:
+        iou (Tensor[N, M]): the NxM matrix containing the pairwise
+            IoU values for every element in boxes1 and boxes2
+    """
+
+    def box_area(box):
+        # box = 4xn
+        return (box[2] - box[0]) * (box[3] - box[1])
+
+    area1 = box_area(box1.T)
+    area2 = box_area(box2.T)
+
+    # inter(N,M) = (rb(N,M,2) - lt(N,M,2)).clamp(0).prod(2)
+    inter = (torch.min(box1[:, None, 2:], box2[:, 2:]) -
+             torch.max(box1[:, None, :2], box2[:, :2])).clamp(0).prod(2)
+    # iou = inter / (area1 + area2 - inter)
+    return inter / (area1[:, None] + area2 - inter)
+
+
+def non_max_suppression(prediction, conf_thres=0.25, iou_thres=0.45,
+                        classes=None, agnostic=False, multi_label=False,
+                        labels=()):
+    """Runs Non-Maximum Suppression (NMS) on inference results
+
+    Args:
+        prediction: (1, 30492, 7)
+    Returns:
+        list of detections, on (n,6) tensor per image [xyxy, conf, cls]
+    """
+
+    # nc = prediction.shape[2] - 5  # number of classes
+    nc = 2
+    x = prediction
+    if not x.shape[0]:
+        return x
+
+    # Settings
+    min_wh, max_wh = 2, 4096  # (pixels) minimum and maximum box width and height
+    max_det = 300  # maximum number of detections per image
+    max_nms = 30000  # maximum number of boxes into torchvision.ops.nms()
+    time_limit = 10.0  # seconds to quit after
+    redundant = True  # require redundant detections
+    multi_label &= nc > 1  # multiple labels per box (adds 0.5ms/img)
+    merge = False  # use merge-NMS
+
+    # Compute conf
+    # for models with one class, cls_loss is 0 and cls_conf is always 0.5,
+    # so there is no need to multiplicate.
+    x[:, 5:] *= x[:, 4:5]  # conf = obj_conf * cls_conf
+
+    # Box (center x, center y, width, height) to (x1, y1, x2, y2)
+    box = xywh2xyxy(x[:, :4])
+
+    # Detections matrix nx6 (xyxy, conf, cls)
+    if multi_label:
+        i, j = (x[:, 5:] > conf_thres).nonzero(as_tuple=False).T
+        x = torch.cat((box[i], x[i, j + 5, None], j[:, None].float()), 1)
+    else:  # best class only
+        conf, j = x[:, 5:].max(1, keepdim=True)
+        x = torch.cat((box, conf, j.float()), 1)[conf.view(-1) > conf_thres]
+
+    # Filter by class
+    if classes is not None:
+        x = x[(x[:, 5:6] == torch.tensor(classes, device=x.device)).any(1)]
+
+    # Check shape
+    n = x.shape[0]  # number of boxes
+    if n > max_nms:  # excess boxes
+        x = x[x[:, 4].argsort(descending=True)[:max_nms]]  # sort by confidence
+
+    # Batched NMS
+    c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
+    boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
+    i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+    if i.shape[0] > max_det:  # limit detections
+        i = i[:max_det]
+
+    return x[i]
 
 
 # Codes from https://github.com/WongKinYiu/yolov7/tree/main
@@ -95,6 +198,8 @@ class LatexDetection(object):
             'outputs': ['output']
         }
 
+        self.conf_thres = kwargs.get('conf_thres', 0.25)
+        self.iou_thres = kwargs.get('iou_thres', 0.45)
         self.categories = {0: 'embedding', 1: 'isolated'}
         self.has_graph_executor = kwargs.get('has_graph_executor', False)
         if not self.has_graph_executor:
@@ -114,12 +219,14 @@ class LatexDetection(object):
         img = cv2.imdecode(img, cv2.IMREAD_COLOR)
 
         prep_outs = self.preprocess(context, [img])
-
+        # with open('/public/bisheng/latex_data/eng2.npy', 'wb') as f:
+        #     np.save(f, prep_outs[0])
         if not self.has_graph_executor:
-            infer_outs = self.graph.run(prep_outs)
+            infer_outs = self.graph.run(prep_outs, ret_type='tensor')
         else:
             graph_executor = context.get('graph_executor')
-            infer_outs = graph_executor.run(self.ys, self.xs, prep_outs)
+            infer_outs = graph_executor.run(
+                self.ys, self.xs, prep_outs, ret_type='tensor')
 
         return self.postprocess(context, infer_outs)
 
@@ -153,10 +260,15 @@ class LatexDetection(object):
 
     def postprocess(
           self, context: Dict[str, Any], inputs: List[Any]) -> Dict[str, Any]:
-        det = inputs[0]
+        det = non_max_suppression(
+            inputs[0].cpu(), self.conf_thres, self.iou_thres).numpy()
+
         box_margin = context.get('box_margin', 2)
         orig_shape = context['orig_shape']
         new_shape = context['new_shape']
+
+        if det[:, :4].shape[0] == 0:
+            return {'result': []}
 
         one_out = []
         det[:, :4] = scale_coords(

python/pybackend_libs/src/tests/test_latex_recog/export_torchscript.py
@@ -165,9 +165,13 @@ class End2End(nn.Module):
     @torch.no_grad()
     def forward(self, x):
         x = self.model(x)
-        print('e2e.x', x.size())
-        x = non_max_suppression(x,
-            conf_thres=self.score_thres, iou_thres=self.iou_thres, nc=self.nc)
+        xc = x[..., 4] > self.score_thres  # candidates
+        x = x[0]
+        x = x[xc[0]]
+
+        # print('e2e.x', x.size())
+        # x = non_max_suppression(x,
+        #     conf_thres=self.score_thres, iou_thres=self.iou_thres, nc=self.nc)
         return x
 
 